d75e3153d2a106db7d8013754d7266b916c645ad
coldexpression <uni_vers_@naver.com>
2021-08-09 22:42:31 +0900

    Spring 순수 JDBC DB Connection

--- demo/src/main/java/com/example/demo/SpringConfig.java
+++ demo/src/main/java/com/example/demo/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // Spring 빈에 직접 등록 하는 설정파일
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +28,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // MemberRepository 는 인터페이스 일 뿐 //
         // MemoryMemberRepository 가 실질적인 저장소 임 //
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
